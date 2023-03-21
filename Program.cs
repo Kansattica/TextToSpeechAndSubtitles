ddbc13f59e71319fcefbb92d345bbefd67b1eb07
Grace L <Grace@hypnovir.us>
2023-03-20 23:46:31 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace TextToSpeechAndSubtitles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                DoTextToSpeech(arg);
+            }
+        }
+
+
+        static void DoTextToSpeech(string textFile)
+        {
+            if (!File.Exists(textFile))
+            {
+                Console.WriteLine($"Hey, buddy, I expect {textFile} to exist and be a text file. Not gonna touch it.");
+                return;
+            }
+
+            var fileInfo = new SynthesisFileInfo(textFile);
+
+            Console.WriteLine($"Going to read {fileInfo.InputFileName} and write to {fileInfo.AudioFileName} and {fileInfo.SubtitleFileName}.");
+
+            var stateThing = new SubtitleSpeechStateThing();
+            stateThing.DoSynthesisAndSubtitles(fileInfo);
+        }
+
+
+
+    }
+}
---
+++ SynthesisFileInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToSpeechAndSubtitles
+{
+    internal class SynthesisFileInfo
+    {
+        public string InputFileName { get; init; }
+        public string SubtitleFileName { get; init; }
+        public string AudioFileName { get; init; }
+        public SynthesisFileInfo(string fileName)
+        {
+            InputFileName = fileName;
+
+            var baseName = Path.GetFileNameWithoutExtension(fileName);
+
+            SubtitleFileName = baseName + ".ass";
+            AudioFileName = baseName + ".wav";
+        }
+    }
+}
