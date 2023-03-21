57f89197e71c6d39d065bc1e4437cf604eef94cd
Grace L <Grace@hypnovir.us>
2023-03-21 10:51:16 -0700

    Leave this as words for now.

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@
 
             Console.WriteLine($"Going to read {fileInfo.InputFileName} and write to {fileInfo.AudioFileName} and {fileInfo.SubtitleFileName}.");
 
-            var stateThing = new SubtitleSpeechStateThing(fileInfo, SubtitleSpeechStateThing.SubtitleOutput.Paragraphs);
+            var stateThing = new SubtitleSpeechStateThing(fileInfo, SubtitleSpeechStateThing.SubtitleOutput.Words);
             stateThing.DoSynthesisAndSubtitles();
             stateThing.MakeWavFile();
         }
